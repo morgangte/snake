024f5bd6bf578c8e25a67daccb57f525bd433346
morgangte <morgan.gillette@ecole.ensicaen.fr>
2024-10-05 12:26:45 +0200

    Snake game moved to its own repo

---
+++ javascript/module/cell.js
@@ -0,0 +1,44 @@
+import { Drawable } from "./drawable.js";
+
+export class Cell extends Drawable {
+    constructor(cv, ctx, x, y, width, heigth, value) {
+        super(cv, ctx, width, heigth);
+
+        this.x = x;
+        this.y = y;
+
+        this.value = value;
+        this.color;
+
+        this.gridDrawn = false;
+    }
+
+    setColor(color, gridDrawn) {
+        this.color = color;
+        this.ctx.fillStyle = color;
+        this.gridDrawn = gridDrawn;
+
+        if (gridDrawn) {
+            this.ctx.fillRect(this.x + 1, this.y + 1, this.width - 1, this.heigth - 1);
+        } else {
+            this.ctx.fillRect(this.x, this.y, this.width, this.heigth);
+        }
+    }
+
+    setValue(value) {
+        this.value = value;
+    }
+
+    touch(value, color, gridDrawn) {
+        this.value = value;
+        this.setColor(color, gridDrawn);
+    }
+
+    getValue() {
+        return this.value;
+    }
+
+    getColor() {
+        return this.color;
+    }
+}
